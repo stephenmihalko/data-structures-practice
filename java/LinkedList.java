3e16532664e0597119f92249d788d41137acc9b5
stephenmihalko <47727340+stephenmihalko@users.noreply.github.com>
2019-03-01 09:23:14 -0500

    Add Eclipse data and update LinkedList
    Write addToEnd and toString

--- java/LinkedList.java
+++ java/LinkedList.java
@@ -10,13 +10,27 @@ public class LinkedList<T>
 	
 	public void addToBeginning(T data)
 	{
+		// Create new node and add the pointer to the old "first" as its next
 		first = new Node(data, first);
 		size++;
 	}
 	
 	public void addToEnd(T data)
 	{
-		
+		if (first == null)
+			this.addToBeginning(data)
+		else
+		{
+			// Get to the last node in the list
+			Node n = first;
+			while (n.next != null)
+				n = n.next;
+			
+			// Create new node and set the next of the previous "last" to the new node
+			n.next = new Node(data)
+			
+			size++;
+		}
 	}
 	
 	public int size() { return size; }
@@ -39,6 +53,25 @@ public class LinkedList<T>
 	public T removeEnd()
 	{
 		
+	}
+	
+	public String toString()
+	{
+		
+		String ret = "| ";
+		Node n = first;
+		if (first == null)
+			return null;
+		else
+		{
+			while (n != null)
+			{
+				ret += n.data.toString() + " | "
+				n = n.next
+			}
+			// Chop off the last space.
+			return ret.substring(0, ret.length()-1);
+		}
 	}
 	
 	public class Node<T>
