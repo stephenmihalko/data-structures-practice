49fefcfa366a3a9d56f0bf07c497d55f1ed48210
stephenmihalko <47727340+stephenmihalko@users.noreply.github.com>
2019-03-01 10:30:15 -0500

    Update empty list tests

--- java/data-structures/src/LinkedListTester.java
+++ java/data-structures/src/LinkedListTester.java
@@ -7,21 +7,26 @@ public class LinkedListTester
 		LinkedList<Integer> begin = new LinkedList<Integer>();
 		LinkedList<Integer> end = new LinkedList<Integer>();
 		
-		System.out.println("====== Testing print empty list ======");
-		System.out.println(begin);
+		System.out.println("====== Empty list ======");
+		System.out.println("List: " + begin);
+		System.out.println("Size: " + begin.size());
+		System.out.println("Get first element: " + begin.getBeginning());
+		System.out.println("Remove first element: " + begin.removeBeginning());
+		System.out.println("New size: " + begin.size());
 		
-		System.out.println("====== Testing add to beginning ======");
-		for (int i = 0; i < 37; i++)
-			begin.addToBeginning(i);
-		System.out.println(begin);
-		System.out.println("Size = " + begin.size());
-		
-		System.out.println("====== Testing add to end ======");
-		for (int i = 0; i < 10; i++)
-			end.addToEnd(i);
-		System.out.println(end);
-		System.out.println("Size = " + end.size());
+//		System.out.println("====== Testing add to beginning ======");
+//		for (int i = 0; i < 37; i++)
+//			begin.addToBeginning(i);
+//		System.out.println(begin);
+//		System.out.println("Size = " + begin.size());
+//		
+//		System.out.println("====== Testing add to end ======");
+//		for (int i = 0; i < 10; i++)
+//			end.addToEnd(i);
+//		System.out.println(end);
+//		System.out.println("Size = " + end.size());
 		
 	}
+	
 
 }
