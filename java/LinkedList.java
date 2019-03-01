419c2518d710dd44a3d9c78ef67f88601df120c3
stephenmihalko <47727340+stephenmihalko@users.noreply.github.com>
2019-03-01 08:50:23 -0500

    Add method declarations for LL class

--- java/LinkedList.java
+++ java/LinkedList.java
@@ -7,6 +7,41 @@ public class LinkedList<T>
 		size = 0;
 	}
 	
+	public void addToBeginning(T data)
+	{
+		
+	}
+	
+	public void addToEnd(T data)
+	{
+		
+	}
+	
+	public int size()
+	{
+		return size;
+	}
+	
+	public T getBeginning()
+	{
+		
+	}
+	
+	public T getEnd()
+	{
+		
+	}
+	
+	public T removeBeginning()
+	{
+		
+	}
+	
+	public T removeEnd()
+	{
+		
+	}
+	
 	public class Node<T>
 	{
 		T data;
