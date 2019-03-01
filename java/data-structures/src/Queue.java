7416ee76160986cc07dad41dadc828e062ec9188
stephenmihalko <47727340+stephenmihalko@users.noreply.github.com>
2019-03-01 12:46:09 -0500

    Write entire Queue class

---
+++ java/data-structures/src/Queue.java
@@ -0,0 +1,76 @@
+/**
+ * 
+ * @author SMihalko
+ *
+ * @param <T>
+ */
+public class Queue<T>
+{
+	/**
+	 * 
+	 */
+	private LinkedList<T> list;
+	
+	/**
+	 * 
+	 */
+	public Queue()
+	{
+		list = new LinkedList<T>();
+	}
+	
+	/**
+	 * 
+	 * @param item
+	 */
+	public void enqueue(T item)
+	{
+		list.addToEnd(item);
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public T dequeue()
+	{
+		return list.removeBeginning();
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public T peek()
+	{
+		return list.getBeginning();
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	public boolean isEmpty()
+	{
+		return list.size() == 0;
+	}
+	
+	/**
+	 * 
+	 * @param item
+	 * @return
+	 */
+	public int search(T item)
+	{
+		return list.search(item);
+	}
+	
+	/**
+	 * 
+	 */
+	public String toString()
+	{
+		String listStr = list.toString();
+		return "<-- " + listStr.substring(2, listStr.length()) + " <--";
+	}
+}
