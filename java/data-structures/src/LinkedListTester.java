02c6633b5fdbc01c283e68037306e9075819fb5c
stephenmihalko <47727340+stephenmihalko@users.noreply.github.com>
2019-03-01 10:38:57 -0500

    Update LinkedListTester.java
    Condensed tests

--- java/data-structures/src/LinkedListTester.java
+++ java/data-structures/src/LinkedListTester.java
@@ -4,28 +4,40 @@ public class LinkedListTester
 
 	public static void main(String[] args)
 	{
-		LinkedList<Integer> begin = new LinkedList<Integer>();
-		LinkedList<Integer> end = new LinkedList<Integer>();
+		LinkedList<Integer> list = new LinkedList<Integer>();
 		
 		System.out.println("====== Empty list ======");
-		System.out.println("List: " + begin);
-		System.out.println("Size: " + begin.size());
-		System.out.println("Get first element: " + begin.getBeginning());
-		System.out.println("Remove first element: " + begin.removeBeginning());
-		System.out.println("New size: " + begin.size());
+		System.out.println("List: " + list);
+		System.out.println("Size: " + list.size());
+		System.out.println("Get first element: " + list.getBeginning());
+		System.out.println("Remove first element: " + list.removeBeginning());
+		System.out.println("New size: " + list.size());
+		System.out.println();
 		
-//		System.out.println("====== Testing add to beginning ======");
-//		for (int i = 0; i < 37; i++)
-//			begin.addToBeginning(i);
-//		System.out.println(begin);
-//		System.out.println("Size = " + begin.size());
-//		
-//		System.out.println("====== Testing add to end ======");
-//		for (int i = 0; i < 10; i++)
-//			end.addToEnd(i);
-//		System.out.println(end);
-//		System.out.println("Size = " + end.size());
+		System.out.println("====== Testing add to beginning ======");
+		for (int i = 1; i < 37; i+=10)
+			list.addToBeginning(i);
+		System.out.println(list);
+		System.out.println("Size: " + list.size());
+		System.out.println();
 		
+		System.out.println("====== Testing add to end ======");
+		for (int i = 14; i < 19; i+=2)
+			list.addToEnd(i);
+		System.out.println(list);
+		System.out.println("Size: " + list.size());
+		System.out.println();
+		
+		System.out.println("====== Testing get beginning ======");
+		System.out.println("First element: " + list.getBeginning());
+		System.out.println("List after getting: " + list);
+		System.out.println();
+		
+		System.out.println("====== Testing remove beginning ======");
+		for (int i = 0; i < 20; i++)
+		{
+			System.out.println(list.removeBeginning() + " size: " + list.size());
+		}
 	}
 	
 
