3c06c7215209219879d316b3c2cda7114b6376b0
stephenmihalko <47727340+stephenmihalko@users.noreply.github.com>
2019-03-01 10:18:15 -0500

    Update LinkedListTester.java
    Test add to beginning, end, and print

--- java/data-structures/src/LinkedListTester.java
+++ java/data-structures/src/LinkedListTester.java
@@ -4,8 +4,22 @@ public class LinkedListTester
 
 	public static void main(String[] args)
 	{
+		LinkedList<Integer> begin = new LinkedList<Integer>();
+		LinkedList<Integer> end = new LinkedList<Integer>();
 		
-
+		System.out.println("====== Testing print empty list ======");
+		System.out.println(begin);
+		
+		System.out.println("====== Testing add to beginning ======");
+		for (int i = 0; i < 10; i++)
+			begin.addToBeginning(i);
+		System.out.println(begin);
+		
+		System.out.println("====== Testing add to end ======");
+		for (int i = 0; i < 10; i++)
+			end.addToEnd(i);
+		System.out.println(end);
+		
 	}
 
 }
